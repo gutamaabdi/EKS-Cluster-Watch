cfa2f9f88c56e16a219694a982c1947f79dc9396
Hiree Abdi <hanan@Hirees-MacBook-Air.local>
2026-09-17 20:48:00 +0100

    infra piplin test

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,7 +1,7 @@
 module "ecr" {
-  source = "terraform-aws-modules/ecr/aws" // this is the blue print, its a written defination of the ecr 
+  source = "terraform-aws-modules/ecr/aws"  
 
-  repository_name = var.c_repo  // this is the name of the ecr contianer were the images will be stored
+  repository_name = var.c_repo 
 
   # repository_read_write_access_arns = ["arn:aws:iam::460576937871:role/github-actions-role"] //this is the iam role that get gives ecr read access.
   repository_lifecycle_policy = jsonencode({ // this is the versioning policy that keeps 30 images only in the ecr contianer 
