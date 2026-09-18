6d64cf43e2827cb61a03a17d799cf1e4ec58c5b0
Hiree Abdi <hanan@Hirees-MacBook-Air.local>
2026-09-18 18:21:34 +0100

    terraform format fix

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -6,7 +6,7 @@ module "eks" {
   kubernetes_version = var.kubernetes_version
 
   # Optional
-  endpoint_public_access = true //not sure what this does
+  endpoint_public_access = true 
 
   # Optional: Adds the current caller identity as an administrator via cluster access entry
   enable_cluster_creator_admin_permissions = true
